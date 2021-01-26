ca97fca26f5e074e2140527783540fc8005b9f19
fifi98 <bel.filip@hotmail.com>
2021-01-26 21:17:33 +0100

    Fixed dropdown filter on iOS

--- mobile/screens/student/Attendance.js
+++ mobile/screens/student/Attendance.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, Platform } from "react-native";
 import { Text, useTheme } from "react-native-paper";
-import { Picker } from "@react-native-picker/picker";
 import { useSelector } from "react-redux";
 import { useIsFocused } from "@react-navigation/native";
+import DropDownPicker from "react-native-dropdown-picker";
 
 import AttendanceItem from "../student/components/AttendanceItem";
 import Loading from "../common/components/Loading";
-
 import api from "../../utils/api";
 
 const moment = require("moment");
@@ -30,13 +29,7 @@ const Attendance = () => {
       .then(({ data }) => {
         setAttendanceData(data.data);
         setFilteredData(
-          data.data.sort((a, b) =>
-            moment(a.fullDate).isBefore(b.fullDate)
-              ? -1
-              : moment(a.fullDate).isAfter(b.fullDate)
-              ? 1
-              : 0
-          )
+          data.data.sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
         );
       })
       .catch((error) => console.log(error))
@@ -51,13 +44,7 @@ const Attendance = () => {
     switch (filterValue) {
       case "Courses":
         setFilteredData(
-          attendanceData.sort((a, b) =>
-            moment(a.fullDate).isBefore(b.fullDate)
-              ? -1
-              : moment(a.fullDate).isAfter(b.fullDate)
-              ? 1
-              : 0
-          )
+          attendanceData.sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
         );
         break;
 
@@ -65,13 +52,7 @@ const Attendance = () => {
         setFilteredData(
           attendanceData
             .filter((item) => item.present === true)
-            .sort((a, b) =>
-              moment(a.fullDate).isBefore(b.fullDate)
-                ? -1
-                : moment(a.fullDate).isAfter(b.fullDate)
-                ? 1
-                : 0
-            )
+            .sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
         );
         break;
 
@@ -80,13 +61,7 @@ const Attendance = () => {
           .get("attendance/missed")
           .then(({ data }) => {
             setFilteredData(
-              data.data.sort((a, b) =>
-                moment(a.fullDate).isBefore(b.fullDate)
-                  ? -1
-                  : moment(a.fullDate).isAfter(b.fullDate)
-                  ? 1
-                  : 0
-              )
+              data.data.sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
             );
           })
           .catch((error) => console.log(error));
@@ -95,18 +70,8 @@ const Attendance = () => {
       case "LastWeek": {
         setFilteredData(
           attendanceData
-            .filter(
-              (item) =>
-                moment().subtract(7, "days").isBefore(item.fullDate) &&
-                moment().isAfter(item.fullDate)
-            )
-            .sort((a, b) =>
-              moment(a.fullDate).isBefore(b.fullDate)
-                ? -1
-                : moment(a.fullDate).isAfter(b.fullDate)
-                ? 1
-                : 0
-            )
+            .filter((item) => moment().subtract(7, "days").isBefore(item.fullDate) && moment().isAfter(item.fullDate))
+            .sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
         );
 
         break;
@@ -115,16 +80,8 @@ const Attendance = () => {
       case "LastMonth":
         setFilteredData(
           attendanceData
-            .filter((item) =>
-              moment().subtract(1, "month").isSame(item.fullDate, "month")
-            )
-            .sort((a, b) =>
-              moment(a.fullDate).isBefore(b.fullDate)
-                ? -1
-                : moment(a.fullDate).isAfter(b.fullDate)
-                ? 1
-                : 0
-            )
+            .filter((item) => moment().subtract(1, "month").isSame(item.fullDate, "month"))
+            .sort((a, b) => (moment(a.fullDate).isBefore(b.fullDate) ? -1 : moment(a.fullDate).isAfter(b.fullDate) ? 1 : 0))
         );
 
         break;
@@ -135,8 +92,8 @@ const Attendance = () => {
     <View style={styles.container}>
       {!loading ? (
         attendanceData.length !== 0 && !loading ? (
-          <View>
-            <View style={{ flexDirection: "row" }}>
+          <View style={{ zIndex: 3 }}>
+            <View style={{ zIndex: 3, flexDirection: "row" }}>
               <Text
                 style={{
                   fontSize: 17,
@@ -147,76 +104,38 @@ const Attendance = () => {
               >
                 Filter by:{" "}
               </Text>
-
-              <View style={{ marginLeft: 10, marginTop: -6 }}>
-                <Picker
-                  selectedValue={selectedFilter}
-                  style={{ height: 50, width: 160 }}
-                  mode={"dialog"}
-                  onValueChange={(itemValue) => {
-                    onChangeFilter(itemValue);
+              <View
+                style={{
+                  ...(Platform.OS !== "android" && {
+                    zIndex: 10,
+                  }),
+                }}
+              >
+                <DropDownPicker
+                  items={[
+                    { label: "Courses", value: "Courses" },
+                    { label: "Attended", value: "Attended" },
+                    { label: "Missed", value: "Missed" },
+                    { label: "Last week", value: "LastWeek" },
+                    { label: "Last month", value: "LastMonth" },
+                  ]}
+                  style={{ backgroundColor: isDarkTheme ? "#272727" : "#fff" }}
+                  arrowColor={isDarkTheme ? "#fff" : "#000"}
+                  labelStyle={{ color: isDarkTheme ? "#fff" : "#000" }}
+                  defaultValue={"Courses"}
+                  containerStyle={{ height: 40, width: 200 }}
+                  dropDownStyle={{ backgroundColor: isDarkTheme ? "#272727" : "#fff" }}
+                  itemStyle={{
+                    justifyContent: "flex-start",
                   }}
-                >
-                  {isDarkTheme ? (
-                    <Picker.Item
-                      color="#a6a6a6"
-                      label="Courses"
-                      value="Courses"
-                    />
-                  ) : (
-                    <Picker.Item label="Courses" value="Courses" />
-                  )}
-
-                  {isDarkTheme ? (
-                    <Picker.Item
-                      color="#a6a6a6"
-                      label="Attended"
-                      value="Attended"
-                    />
-                  ) : (
-                    <Picker.Item label="Attended" value="Attended" />
-                  )}
-
-                  {isDarkTheme ? (
-                    <Picker.Item
-                      color="#a6a6a6"
-                      label="Missed"
-                      value="Missed"
-                    />
-                  ) : (
-                    <Picker.Item label="Missed" value="Missed" />
-                  )}
-
-                  {isDarkTheme ? (
-                    <Picker.Item
-                      color="#a6a6a6"
-                      label="Last week"
-                      value="LastWeek"
-                    />
-                  ) : (
-                    <Picker.Item label="Last week" value="LastWeek" />
-                  )}
-
-                  {isDarkTheme ? (
-                    <Picker.Item
-                      color="#a6a6a6"
-                      label="Last month"
-                      value="LastMonth"
-                    />
-                  ) : (
-                    <Picker.Item label="Last month" value="LastMonth" />
-                  )}
-                </Picker>
+                  onChangeItem={(item) => onChangeFilter(item.value)}
+                />
               </View>
             </View>
 
             {filteredData.length !== 0 ? (
               <View style={{ height: "94%" }}>
-                <FlatList
-                  keyExtractor={(item) => item.id}
-                  data={filteredData}
-                  renderItem={({ item }) => <AttendanceItem item={item} />}
-                />
+                <FlatList keyExtractor={(item) => item.id} data={filteredData} renderItem={({ item }) => <AttendanceItem item={item} />} />
               </View>
             ) : (
               <View
@@ -240,18 +159,10 @@ const Attendance = () => {
             )}
           </View>
         ) : (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             <View>
-              <Text
-                style={{ fontSize: 19, fontWeight: "500", marginBottom: 20 }}
-              >
-                You don't have any attendance record yet!
-              </Text>
-              <Text style={{ textAlign: "center" }}>
-                Attendances will be displayed after your first attendance.
-              </Text>
+              <Text style={{ fontSize: 19, fontWeight: "500", marginBottom: 20 }}>You don't have any attendance record yet!</Text>
+              <Text style={{ textAlign: "center" }}>Attendances will be displayed after your first attendance.</Text>
             </View>
           </View>
         )
