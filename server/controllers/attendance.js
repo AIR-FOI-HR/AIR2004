9f77082eb1058ac49735873562f02d1b8fedd1de
fifi98 <bel.filip@hotmail.com>
2021-02-02 21:46:44 +0100

    Fixed marking attendance

--- server/controllers/attendance.js
+++ server/controllers/attendance.js
@@ -14,11 +14,7 @@ exports.add = async (req, res) => {
       ...req.body,
     }).save();
     const lectureId = req.body.lecture;
-    Lecture.findOneAndUpdate(
-      { _id: lectureId },
-      { $push: { attendingStudents: req.body.user } },
-      function (err, affected, resp) {}
-    );
+    Lecture.findOneAndUpdate({ _id: lectureId }, { $push: { attendingStudents: req.body.user } }, function (err, affected, resp) {});
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(400).json({ success: false, error });
@@ -38,9 +34,7 @@ exports.update = async (req, res) => {
 
 exports.delete = async (req, res) => {
   try {
-    const attendance = await (
-      await Attendance.findById(req.params.id)
-    ).deleteOne();
+    const attendance = await (await Attendance.findById(req.params.id)).deleteOne();
     const data = attendance.toJSON();
     res.status(200).json({ success: true, data });
   } catch (error) {
@@ -106,15 +100,9 @@ exports.getMissed = async (req, res) => {
 
     const missed = missedAttendance
       .map((item) => {
-        if (
-          item.course.enrolledStudents.includes(student[0]._id) &&
-          !item.attendingStudents.includes(student[0]._id)
-        )
-          return item;
+        if (item.course.enrolledStudents.includes(student[0]._id) && !item.attendingStudents.includes(student[0]._id)) return item;
       })
-      .filter(
-        (item) => item !== undefined && item.course.name === "Matematics 2"
-      );
+      .filter((item) => item !== undefined && item.course.name === "Matematics 2");
 
     const data = missed.map((attendance) => {
       return {
@@ -136,23 +124,21 @@ exports.getMissed = async (req, res) => {
 
 exports.markAttendance = async (req, res) => {
   const code = req.body.code;
-  const user = req.body.user;
+  const userId = req.body.user;
 
   try {
     const foundAttendance = await Attendance.findOne({ qrCode: code });
 
-    const lectureInProgress = global.lecturesInProgress.find(
-      (x) => x.lecture == foundAttendance.lecture
-    );
+    const lectureInProgress = global.lecturesInProgress.find((x) => x.lecture == foundAttendance.lecture);
 
     // Check if that user has already marked attendance on that lecture
     const alreadyMarked = await Attendance.findOne({
       lecture: lectureInProgress.lecture,
-      user,
+      user: userId,
     });
 
     // Check if that user is enrolled to course
-    const user = await User.findOne({ email: user.email });
+    const user = await User.findById(userId);
     const lecture = await Lecture.findById(lectureInProgress.lecture);
 
     const enrolled = user.enrolledCourses.includes(lecture.course);
@@ -164,7 +150,7 @@ exports.markAttendance = async (req, res) => {
     // Update attendance document with the code
     const attendance = await Attendance.findOneAndUpdate(
       { qrCode: code, user: null },
-      { $set: { user, modifiedAt: Date.now() } },
+      { $set: { user: userId, modifiedAt: Date.now() } },
       { new: true }
     );
 
@@ -176,10 +162,8 @@ exports.markAttendance = async (req, res) => {
     if (!attendanceToken) return res.status(400).json({ success: false });
 
     // Add user into array of students that submit attendance for this lecture
-    lectureToRecord.attendingStudents = [
-      ...lectureToRecord.attendingStudents,
-      user,
-    ];
+    lectureToRecord.attendingStudents.push(userId);
+
     await lectureToRecord.save();
 
     // If the update didn't succeed (the qrCode is either invalid or it has been already used) return 400
@@ -197,14 +181,8 @@ exports.markAttendance = async (req, res) => {
     });
 
     // Send the attendance to the mobile app along with the user data who marked the attendance
-    const markedAttendance = await Attendance.findById(attendance.id).populate(
-      "user",
-      "name surname"
-    );
-    global.io
-      .of("/teacher")
-      .to(attendanceToken)
-      .emit("new attendance", markedAttendance);
+    const markedAttendance = await Attendance.findById(attendance.id).populate("user", "name surname");
+    global.io.of("/teacher").to(attendanceToken).emit("new attendance", markedAttendance);
 
     res.status(200).json({ success: true });
   } catch (error) {
